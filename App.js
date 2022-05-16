b88efcf6ca5a34e3526c02ca6add3a6e0a82c6d5
Kei <dkk1286@iclud.com>
2022-05-16 18:35:29 +0900

    tfjs-react-nativeのsiryou 使用

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform} from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Camera } from 'expo-camera';
 
+// new imports
+import * as tf from "@tensorflow/tfjs";
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  // initialisation of the new camera outside of the component
+  const TensorCamera = cameraWithTensors(Camera);
 
   useEffect(() => {
     (async () => {
@@ -22,9 +28,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Camera 
+      <TensorCamera 
         style={styles.camera} 
         type={Camera.Constants.Type.back}
+        onReady={() => {}}
+        resizeHeight={200}
+        resizeWidth={152}
+        resizeDepth={3}
+        autorender={true}
+        cameraTextureHeight={textureDims.height}
+        cameraTextureWidth={textureDims.width}
       />
     </View>
   );
@@ -38,3 +51,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+const textureDims = Platform.OS === 'ios' ?
+{
+  height: 1920,
+  width: 1080,
+} :
+{
+  height: 1200,
+  width: 1600,
+};
