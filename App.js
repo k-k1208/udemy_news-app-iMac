7339025d85d32914e24d59dde97efb4f8d1abbf8
Kei <dkk1286@iclud.com>
2022-05-16 17:24:02 +0900

    Camera.requestCameraPermissionsAsync()に変更

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
